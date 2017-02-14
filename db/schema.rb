a9e79632e8c2bbde0c9639b5caeee5e72599d663
sirgill <sunnygill@Surinders-MacBook-Pro.local>
2017-02-14 11:01:59 -0500

    Added stock-qoute gem and created stock model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214150922) do
+ActiveRecord::Schema.define(version: 20170214154450) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string   "tocker"
+    t.string   "name"
+    t.decimal  "last_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
